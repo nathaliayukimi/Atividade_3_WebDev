092be6d6911d51c707020518bee747891a0b8d83
nathaliayukimi <129632321+nathaliayukimi@users.noreply.github.com>
2023-06-18 21:02:03 -0300

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,24 @@
+function is_even(a){
+    if (a % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+function mul(a, b){
+    return a * b;
+}
